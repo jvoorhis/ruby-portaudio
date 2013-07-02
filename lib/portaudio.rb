14167c53ad7cc48484538ed17fe71ce80e4120c3
Jeremy Voorhis <jvoorhis@gmail.com>
2013-07-01 19:43:55 -0700

    Whitespace

--- lib/portaudio.rb
+++ lib/portaudio.rb
@@ -3,7 +3,7 @@ require 'ffi'
 module PortAudio
   module C
     extend FFI::Library
-    
+
     case RUBY_PLATFORM
     when /darwin/i
       ffi_lib '/System/Library/Frameworks/AudioUnit.framework/AudioUnit'
@@ -11,19 +11,19 @@ module PortAudio
       ffi_lib '/System/Library/Frameworks/AudioToolbox.framework/AudioToolbox'
       ffi_lib '/System/Library/Frameworks/CoreServices.framework/CoreServices'
     end
-    
+
     ffi_lib 'portaudio'
-    
+
     PA_ERROR = :int
     PA_NO_ERROR = 0
-    
+
     PA_DEVICE_INDEX = :int
     PA_NO_DEVICE = (2 ** FFI::Platform::LONG_SIZE) - 1
-    
+
     PA_HOST_API_TYPE_ID = :int
-    
+
     PA_HOST_API_INDEX = :int
-    
+
     class PaHostApiInfo < FFI::Struct
       layout :struct_version, :int,
              :type, PA_HOST_API_TYPE_ID,
@@ -32,15 +32,15 @@ module PortAudio
              :default_input_device, PA_DEVICE_INDEX,
              :default_output_device, PA_DEVICE_INDEX
     end
-    
+
     class PaHostErrorInfo < FFI::Struct
       layout :host_api_type, PA_HOST_API_TYPE_ID,
              :error_code, :long,
              :error_text, :string
     end
-    
+
     PA_TIME = :double
-    
+
     PA_SAMPLE_FORMAT = :ulong
     PA_SAMPLE_FORMAT_MAP = {
       :float32 => 0x00000001,
@@ -51,9 +51,9 @@ module PortAudio
       :uint8   => 0x00000020,
       :custom  => 0x00010000
     }
-    
+
     PA_NON_INTERLEAVED = 0x80000000
-    
+
     class PaDeviceInfo < FFI::Struct
       layout :struct_version, :int,
              :name, :string,
@@ -66,14 +66,14 @@ module PortAudio
              :default_high_output_latency, PA_TIME,
              :default_sample_rate, :double
     end
-    
+
     class PaStreamParameters < FFI::Struct
       layout :device, PA_DEVICE_INDEX,
              :channel_count, :int,
              :sample_format, PA_SAMPLE_FORMAT,
              :suggested_latency, PA_TIME,
              :host_specific_stream_info, :pointer
-      
+
       def self.from_options(options)
         params = C::PaStreamParameters.new
         params[:device] = case options[:device]
@@ -85,11 +85,11 @@ module PortAudio
         params
       end
     end
-    
+
     PA_FORMAT_IS_SUPPORTED = 0
-    
+
     PA_FRAMES_PER_BUFFER_UNSPECIFIED = 0
-    
+
     PA_STREAM_FLAGS = :ulong
     PA_NO_FLAG                 = 0
     PA_CLIP_OFF                = 0x00000001
@@ -98,30 +98,30 @@ module PortAudio
     PA_PRIME_OUTPUT_BUFFERS_USING_STREAM_CALLBACK =
                                  0x00000008
     PA_PLATFORM_SPECIFIC_FLAGS = 0xFFFF0000
-    
+
     PA_STREAM_CALLBACK_FLAGS = :ulong
     PA_INPUT_UNDERFLOW  = 0x00000001
     PA_INPUT_OVERFLOW   = 0x00000002
     PA_OUTPUT_UNDERFLOW = 0x00000004
     PA_OUTPUT_OVERFLOW  = 0x00000008
     PA_PRIMING_OUTPUT   = 0x00000010
-    
+
     PA_STREAM_CALLBACK_RESULT = :int
     PA_CONTINUE = 0
     PA_COMPLETE = 1
     PA_ABORT    = 2
-    
+
     PA_STREAM_CALLBACK = :pointer
-    
+
     PA_STREAM_FINISHED_CALLBACK = :pointer
-    
+
     class PaStreamInfo < FFI::Struct
       layout :struct_version, :int,
              :input_latency, PA_TIME,
              :output_latency, PA_TIME,
              :sample_rate, :double
     end
-    
+
     attach_function :Pa_GetVersion, [], :int
     attach_function :Pa_GetVersionText, [], :string
     attach_function :Pa_GetErrorText, [PA_ERROR], :string
@@ -157,37 +157,37 @@ module PortAudio
     attach_function :Pa_GetSampleSize, [:ulong], PA_ERROR
     attach_function :Pa_Sleep, [:long], :void
   end
-  
+
   def version_number
     C.Pa_GetVersion()
   end
   module_function :version_number
-  
+
   def version_text
     C.Pa_GetVersionText()
   end
   module_function :version_text
-  
+
   def error_text(pa_err)
     C.Pa_GetErrorText(pa_err)
   end
   module_function :error_text
-  
+
   def init
     C.Pa_Initialize()
   end
   module_function :init
-  
+
   def terminate
     C.Pa_Terminate()
   end
   module_function :terminate
-  
+
   def sleep(msec)
     C.Pa_Sleep(msec)
   end
   module_function :sleep
-  
+
   def invoke
     status = yield
     if status != C::PA_NO_ERROR
@@ -195,7 +195,7 @@ module PortAudio
     end
   end
   module_function :invoke
-  
+
   def sample_size(format)
     status = C.Pa_GetSampleSize(C::PA_SAMPLE_FORMAT_MAP[format])
     if status >= 0 then status
@@ -204,20 +204,20 @@ module PortAudio
     end
   end
   module_function :sample_size
-  
+
   class Host
     def self.count
       C.Pa_GetHostApiCount()
     end
-    
+
     def self.default
       new(C.Pa_GetDefaultHostApi())
     end
-    
+
     class << self
       private :new
     end
-    
+
     def initialize(index)
       @index = index
       infop = C.Pa_GetHostApiInfo(@index)
@@ -227,135 +227,135 @@ module PortAudio
       end
       @info = C::PaHostApiInfo.new(infop) unless infop.null?
     end
-    
+
     def name
       @info[:name]
     end
-    
+
     def devices
       @devices ||= DeviceCollection.new(@index, @info)
     end
-    
+
     class DeviceCollection
       include Enumerable
-      
+
       def initialize(host_index, host_info)
         @host_index, @host_info = host_index, host_info
       end
-      
+
       def count
         @host_info[:device_count]
       end
       alias_method :size, :count
-      
+
       def [](index)
         case index
         when (0 ... count)
           Device.new(C.Pa_HostApiDeviceIndexToDeviceIndex(@host_index, index))
         end
       end
-      
+
       def each
         0.upto(count) { |i| yield self[i] }
       end
-      
+
       def default_input
         index = @host_info[:default_input_device]
         self[index] unless C::PA_NO_DEVICE == index
       end
-      
+
       def default_output
         index = @host_info[:default_output_device]
         self[index] unless C::PA_NO_DEVICE == index
       end
     end
   end
-  
+
   class Device
     def self.count
       C.Pa_GetDeviceCount()
     end
-    
+
     def self.default_input
       index = C.Pa_GetDefaultInputDevice()
       new(index) unless C::PA_NO_DEVICE == index
     end
-    
+
     def self.default_output
       index = C.Pa_GetDefaultOutputDevice()
       new(index) unless C::PA_NO_DEVICE == index
     end
-    
+
     def initialize(index)
       @index = index
       infop = C.Pa_GetDeviceInfo(@index)
       raise RuntimeError, "Device not found" if infop.null?
       @info = C::PaDeviceInfo.new(infop)
     end
-    
+
     attr_reader :index
-    
+
     def name
       @info[:name]
     end
-    
+
     def max_input_channels
       @info[:max_input_channels]
     end
-    
+
     def max_output_channels
       @info[:max_output_channels]
     end
-    
+
     def default_low_input_latency
       @info[:default_low_input_latency]
     end
-    
+
     def default_low_output_latency
       @info[:default_low_output_latency]
     end
-    
+
     def default_high_input_latency
       @info[:default_high_input_latency]
     end
-    
+
     def default_high_output_latency
       @info[:default_high_output_latency]
     end
-    
+
     def default_sample_rate
       @info[:default_sample_rate]
     end
   end
-  
+
   class Stream
     def self.format_supported?(options)
       if options[:input]
         in_params = C::PaStreamParameters.from_options(options[:input])
       end
-      
+
       if options[:output]
         out_params = C::PaStreamParameters.from_options(options[:output])
       end
-      
+
       sample_rate = options[:sample_rate]
       err = C.Pa_IsFormatSupported(in_params, out_params, sample_rate)
-      
+
       case err
         when C::PA_FORMAT_IS_SUPPORTED then true
         else false
       end
     end
-    
+
     def self.open(options)
       if options[:input]
         in_params = C::PaStreamParameters.from_options(options[:input])
       end
-      
+
       if options[:output]
         out_params = C::PaStreamParameters.from_options(options[:output])
       end
-      
+
       sample_rate = options[:sample_rate]
       frames    = options[:frames]    || C::PA_FRAMES_PER_BUFFER_UNSPECIFIED
       flags     = options[:flags]     || C::PA_NO_FLAG
@@ -368,38 +368,38 @@ module PortAudio
             sample_rate, frames, flags,
             callbackp, user_data)
         }
-        
+
         return new(streamp.read_pointer)
       end
     end
-    
+
     class << self
       private :new
     end
-    
+
     def initialize(pointer)
       @stream = pointer
       infop = C.Pa_GetStreamInfo(@stream)
       raise RuntimeError, "Invalid stream" if infop.null?
       @info = C::PaStreamInfo.new(infop)
     end
-    
+
     def close
       PortAudio.invoke { C.Pa_CloseStream(@stream) }
     end
-    
+
     def start
       PortAudio.invoke { C.Pa_StartStream(@stream) }
     end
-    
+
     def stop
       PortAudio.invoke { C.Pa_StopStream(@stream) }
     end
-    
+
     def abort
       PortAudio.invoke { C.Pa_AbortStream(@stream) }
     end
-    
+
     def stopped?
       status = C.Pa_IsStreamStopped(@stream)
       case status
@@ -409,7 +409,7 @@ module PortAudio
           raise RuntimeError, PortAudio.error_text(status)
       end
     end
-    
+
     def active?
       status = C.Pa_IsStreamActive(@stream)
       case status
@@ -419,29 +419,29 @@ module PortAudio
           raise RuntimeError, PortAudio.error_text(status)
       end
     end
-    
+
     def time
       C.Pa_GetStreamTime(@stream)
     end
-    
+
     def cpu_load
       C.Pa_GetStreamCpuLoad(@stream)
     end
-    
+
     def read
       raise NotImplementedError, "Stream#read is not implemented" # TODO ;)
     end
-    
+
     def write(buffer)
       C.Pa_WriteStream(@stream, buffer.to_ptr, buffer.frames)
     end
     alias_method :<<, :write
   end
-  
+
   # A memory buffer for interleaved PCM data
   class SampleBuffer
     attr_reader :channels, :format, :frames, :size
-    
+
     def initialize(options = {})
       @channels, @format, @frames = options.values_at(:channels, :format, :frames)
       @sample_size = PortAudio.sample_size(@format)
@@ -449,16 +449,16 @@ module PortAudio
       @size = @sample_size * @channels * @frames
       @buffer = FFI::MemoryPointer.new(@size)
     end
-    
+
     def dispose
       @buffer.free
       nil
     end
-    
+
     def to_ptr
       @buffer
     end
-    
+
     def [](frame, channel)
       index = (channel * @sample_size) + (frame * @frame_size)
       case @format
@@ -468,7 +468,7 @@ module PortAudio
         raise NotImplementedError, "Unsupported sample format #{@format}"
       end
     end
-    
+
     def []=(frame, channel, sample)
       index = (channel * @sample_size) + (frame * @frame_size)
       case @format
@@ -478,7 +478,7 @@ module PortAudio
         raise NotImplementedError, "Unsupported sample format #{@format}"
       end
     end
-    
+
     def fill
       samples = []
       for frame in 0 ... @frames
@@ -494,12 +494,12 @@ end
 
 if __FILE__ == $0
   PortAudio.init
-  
+
   block_size = 1024
   sr   = 44100
   step = 1.0/sr
   time = 0.0
-  
+
   stream = PortAudio::Stream.open(
              :sample_rate => sr,
              :frames => block_size,
@@ -508,26 +508,26 @@ if __FILE__ == $0
                :channels => 1,
                :sample_format => :float32
               })
-  
+
   buffer = PortAudio::SampleBuffer.new(
              :format   => :float32,
              :channels => 1,
              :frames   => block_size)
-  
+
   playing = true
   Signal.trap('INT') { playing = false }
   puts "Ctrl-C to exit"
-  
+
   stream.start
-  
+
   loop do
     stream << buffer.fill { |frame, channel|
       time += step
       Math.cos(time * 2 * Math::PI * 440.0) * Math.cos(time * 2 * Math::PI)
     }
-    
+
     break unless playing
   end
-  
+
   stream.stop
 end
